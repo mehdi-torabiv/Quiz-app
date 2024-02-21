8bfaf3bfd5b2281eeccc42cbee1df7940ce39276
zuies <zurabichkhikvadze66@gmail.com>
2024-02-21 13:50:30 +0300

    feat/implement-typographi-component

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,12 @@
 <script lang="ts" setup>
 import FButton from '../components/shared/FButton.vue';
+import FTypographi from '../components/shared/FTypographi.vue';
 </script>
 
 <template>
   <div class="container">
     <FButton color-class="bg-secondary" class="text-white">Start Quiz</FButton>
+    <FTypographi tag="p" size="5xl">ds</FTypographi>
   </div>
 </template>
 
