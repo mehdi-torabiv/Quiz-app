1b6bce42ff71e5b98f4cbcd41f83fe107dab9cac
zuies <zurabichkhikvadze66@gmail.com>
2024-02-21 11:16:08 +0300

    feat/add-mixin-and-classes

--- app.vue
+++ app.vue
@@ -1,5 +1,9 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <p class="text-sm">This is small text.</p>
+    <p class="text-md">This is medium text.</p>
+    <p class="text-lg">This is large text.</p>
+    <p class="text-xl">This is extra-large text.</p>
+    <p class="text-2xl">This is 2x extra-large text.</p>
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  css: ['./assets/styles/_main.less'],
   vite: {
     css: {
       preprocessorOptions: {
